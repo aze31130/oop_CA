d6d82dd9979dc2ad5839fac05029999a68f21133
Guilhem <guilhem.carlet@epita.fr>
2021-04-19 16:56:42 +0100

    Updated database tables

--- Data/Context.cs
+++ Data/Context.cs
@@ -6,6 +6,11 @@ namespace oop_CA.Data
     public class Context : DbContext
     {
         public Context(DbContextOptions<Context> options) : base(options) { }
+        public DbSet<Attendance> attendances { get; set; }
+        public DbSet<Course> courses { get; set; }
+        public DbSet<Group> groups { get; set; }
+        public DbSet<Mark> marks { get; set; }
+        public DbSet<Timetable> timetables { get; set; }
         public DbSet<User> users { get; set; }
     }
 }
