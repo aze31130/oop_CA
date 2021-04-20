864a0f9cfa224802fde39b89262900c819137e40
Guilhem <guilhem.carlet@epita.fr>
2021-04-20 11:40:26 +0100

    Added hash password method

--- Controllers/UsersController.cs
+++ Controllers/UsersController.cs
@@ -5,6 +5,8 @@ using oop_CA.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace oop_CA.Controllers
@@ -26,10 +28,43 @@ namespace oop_CA.Controllers
         {
             return View();
         }
+        public IActionResult Register()
+        {
+            return View();
+        }
 
+        //Post method to register a student
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> registerUser([Bind("id,firstname,lastname,email,password,userType")] User user)
+        {
+            user.password = getSHA256Hash(user.password);
+            user.amounttopay = 0;
+            user.payedamount = 0;
+            user.groupId = 0;
 
+            if (ModelState.IsValid)
+            {
+                _context.users.Add(user);
+                await _context.SaveChangesAsync();
+            }
+            return View("Index");
+        }
 
-        
+        private string getSHA256Hash(string password)
+        {
+            using (SHA256 sha256Hash = SHA256.Create())
+            {
+                byte[] bytes = sha256Hash.ComputeHash(Encoding.UTF8.GetBytes(password));
+                StringBuilder builder = new StringBuilder();
+                for (int i = 0; i < bytes.Length; i++)
+                {
+                    builder.Append(bytes[i].ToString("x2"));
+                }
+                return builder.ToString();
+            }
+        }
+
         [HttpGet]
         public async Task<ActionResult<IEnumerable<User>>> GetUsers()
         {
@@ -81,6 +116,5 @@ namespace oop_CA.Controllers
                 return user;
             }
         }
-        
     }
 }
--- Models/User.cs
+++ Models/User.cs
@@ -14,6 +14,7 @@
         public int groupId { get; set; }
         public string firstname { get; set; }
         public string lastname { get; set; }
+        public string email { get; set; }
         public string password { get; set; }
         public USER_TYPE userType { get; set; }
         public int amounttopay { get; set; }
